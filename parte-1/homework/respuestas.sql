40a1c50b6cd4043b41f8ad78534019572c53bdff
DataEngIII2023 <126905289+DataEngIII2023@users.noreply.github.com>
2023-05-01 14:51:15 -0300

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -1,4 +1,4 @@
---CLASE 1:
+--------------------------------------------CLASE 1:----------------------------------------------------------------------
 --1
 SELECT * FROM stg.product_master
 WHERE  categoria = 'Electro'
@@ -72,8 +72,7 @@ FROM stg.order_line_sale
 
 WHERE  fecha between '2022-10-01' and '2022-11-10'
 
-
-CLASE 2:
+--------------------------------------------CLASE 2:----------------------------------------------------------------------
 --1 Cuales son los paises donde la empresa tiene tiendas?
 SELECT distinct pais
 FROM stg.store_master
@@ -162,7 +161,7 @@ group by orden
 order by orden
 
 
-CLASE 3:
+--------------------------------------------CLASE 3:----------------------------------------------------------------------
 --1. Mostrar nombre y codigo de producto, categoria y color para todos los productos de la marca Philips y Samsung, 
 --mostrando la leyenda "Unknown" cuando no hay un color disponible
 SELECT nombre,codigo_producto,categoria,COALESCE(color,'Unknown')
@@ -197,16 +196,55 @@ group by PM.subcategoria,OLS.cantidad,ubicacion_producto
 order by ubicacion_producto 
 
 --5. Mostrar una vista donde sea vea el nombre de tienda y la cantidad de entradas de personas que hubo desde la fecha de apertura para el sistema "super_store".
+SELECT SM.nombre, SM.fecha_apertura, SUM(SSC.conteo) as cantidad_entrada_personas
+from stg.store_master as SM
+left join stg.super_store_count as SSC
+on SM.codigo_tienda=SSC.tienda
+group by SM.nombre, SM.fecha_apertura
 
 --6. Cual es el nivel de inventario promedio en cada mes a nivel de codigo de producto y tienda; mostrar el resultado con el nombre de la tienda.
+select SM.nombre, to_char(INV.fecha,'YYYY-MM') as mes,INV.sku as codigo_producto,AVG((coalesce(INV.final,0)+coalesce(INV.inicial))/2) as promedio_venta
+from stg.store_master as SM
+left join stg.inventory as INV
+on SM.codigo_tienda=INV.tienda
+group by SM.nombre, mes, codigo_producto
+order by SM.nombre
 
 --7. Calcular la cantidad de unidades vendidas por material. Para los productos que no tengan material usar 'Unknown', homogeneizar los textos si es necesario.
+select coalesce(UPPER(PM.material),'Unknown') as material_producto, SUM(OLS.cantidad) as cantidades_vendidas
+from stg.product_master as PM
+left join stg.order_line_sale as OLS
+on PM.codigo_producto=OLS.producto
+group by material_producto
 
 --8. Mostrar la tabla order_line_sales agregando una columna que represente el valor de venta bruta en cada linea convertido a dolares usando la tabla de tipo de cambio.
+SELECT 
+OLS.*,
+case when OLS.moneda = 'ARS' then OLS.venta/AFR.cotizacion_usd_peso
+     when OLS.moneda = 'URU' then OLS.venta/AFR.cotizacion_usd_uru
+     when OLS.moneda = 'EUR' then OLS.venta/AFR.cotizacion_usd_eur
+end AS valor_en_dolares
+from stg.order_line_sale as OLS
+left join stg.monthly_average_fx_rate as AFR
+on date_part('month',OLS.fecha)=date_part('month',AFR.mes)
+order by OLS.orden
 
 --9. Calcular cantidad de ventas totales de la empresa en dolares.
 
 --10. Mostrar en la tabla de ventas el margen de venta por cada linea. Siendo margen = (venta - descuento) - costo expresado en dolares.
+SELECT 
+OLS.*,
+case when OLS.moneda = 'ARS' then (coalesce(OLS.venta,0)+coalesce(OLS.descuento,0))/AFR.cotizacion_usd_peso -- Se pone positivo ya que el valor de los descuentos se encuentra negativo en la tabla
+     when OLS.moneda = 'URU' then (coalesce(OLS.venta,0)+coalesce(OLS.descuento,0))/AFR.cotizacion_usd_uru
+     when OLS.moneda = 'EUR' then (coalesce(OLS.venta,0)+coalesce(OLS.descuento,0))/AFR.cotizacion_usd_eur
+end AS margen_en_dolares
+from stg.order_line_sale as OLS
+left join stg.monthly_average_fx_rate as AFR
+on date_part('month',OLS.fecha)=date_part('month',AFR.mes)
+order by OLS.orden
 
 --11. Calcular la cantidad de items distintos de cada subsubcategoria que se llevan por numero de orden.
+
+
+--------------------------------------------CLASE 4:----------------------------------------------------------------------
 
